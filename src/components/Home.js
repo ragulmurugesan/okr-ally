bcb02333dd42762c92c01d0398f7f83eb707271d
Ragul Murugesan <y509198@softwareag.com>
2021-07-12 17:50:02 +0530

    Home component has been converted from Class component to Functional component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { SAMPLE_OKR } from '../constants/okr.constant';
 import OkrSegment from './OkrSegment';
 
-export default class Home extends Component {
+export default function Home() {
 
-    constructor() {
-        super();
-        this.state = { okrData: {} };
-    }
+    const [state, setState] = useState({ okrData: {} });
 
-    componentDidMount() {
+    const fetchData = () => {
+        console.log('fetchData');
         fetch(SAMPLE_OKR)
             .then(res => res.json())
-            .then(value => this.constructJSON(value.data));
+            .then(value => constructJSON(value.data));
     }
 
-    constructJSON(data) {
+    const constructJSON = (data) => {
         let cache = {};
         const misc = 'MISC000';
         cache[misc] = { title: 'Miscellanous', children: [] };
@@ -27,19 +25,21 @@ export default class Home extends Component {
             else if (cache[parentId]) cache[parentId].children.push(item);
             else cache[misc].children.push(item);
         })
-        this.setState({ okrData: cache });
+        setState({ okrData: cache });
     }
 
-    render() {
-        return (
-            <>
-                <h1 className="txt-center">Ally.io - OKR</h1>
-                <section className="ally-okr">
-                    {Object.keys(this.state.okrData)
-                        .map(parentId => <OkrSegment key={parentId} title={this.state.okrData[parentId].title} nestChildren={this.state.okrData[parentId].children} />)}
-                </section>
-            </>
-        )
-    }
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+    return (
+        <>
+            <h1 className="txt-center">Ally.io - OKR</h1>
+            <section className="ally-okr">
+                {Object.keys(state.okrData)
+                    .map(parentId => <OkrSegment key={parentId} title={state.okrData[parentId].title} nestChildren={state.okrData[parentId].children} />)}
+            </section>
+        </>
+    )
 }
 
