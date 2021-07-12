6e401ec8c5097a5e33f3631794dfb3c365d39eae
Ragul Murugesan <y509198@softwareag.com>
2021-07-12 18:20:04 +0530

    1. Accordion Implementation 2. Added pngs for icon

--- src/components/okrSegment.js
+++ src/components/okrSegment.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function OkrSegment({ title, nestChildren }) {
+
+    const [show, setShow] = useState(true);
+
     return (
         <div className="ally-okr__segment">
-            <h3 className="ally-okr__segment-title">{title}</h3>
-            <ul>
-                {nestChildren.map(child => <li key={child.id} className="ally-okr__segment-item">{child.title}</li>)}
-            </ul>
+            <h3 className="ally-okr__segment-title okr-accordion">
+                <i className={show ? 'accordion-up' : 'accordion-down'} onClick={() => setShow(!show)}></i>
+                {title}
+            </h3>
+            {show &&
+                <ul>
+                    {nestChildren.map(child => <li key={child.id} className="ally-okr__segment-item">{child.title}</li>)}
+                </ul>
+            }
         </div>
     )
 }
